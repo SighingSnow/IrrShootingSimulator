19090c3fb7be1b750aed53573c21b79e6caea21a
MagicAtom <1263750383@qq.com>
2021-02-09 12:07:45 +0800

    Add API of picture transform.

---
+++ source/src/vedioManager.h
@@ -0,0 +1,22 @@
+#ifndef VEDIO_MANAGER_h
+#define VEDIO_MANAGER_h
+
+#include "stb_image.h"
+
+struct SceenShoot
+{
+    char* pictureData;
+    
+    //TODO: read file from local and turn to bitStream
+    void pictureToData();
+    
+    //TODO: display the picture from local
+    void display();
+    
+    //TODO: ture bitSteam to a picture
+    void dataToPicture();
+    
+};
+
+#endif
+
